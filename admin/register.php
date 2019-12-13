f310e795bcebc2bd0d41895e788ccb3cd2f68012
Luis Naranjo <luismigeek@gmail.com>
2019-12-12 19:37:01 -0500

    Register listo

--- admin/register.php
+++ admin/register.php
@@ -1,3 +1,34 @@
+<?php
+
+require_once "../includes/db/database.php";
+$db = new Conexion();
+
+session_start();
+
+if (isset($_SESSION["type"]) && $_SESSION["type"] == 1) {
+    header("Location: home.php");
+}
+
+if (!empty($_POST)) {
+
+    $email = (isset($_POST['inputEmail'])) ? $_POST['inputEmail'] : null ;
+    $passw = (isset($_POST['inputPassword'])) ? $_POST['inputPassword'] : null ;
+    $passw2 = (isset($_POST['confirmPassword'])) ? $_POST['confirmPassword'] : null ;
+
+    $error = '';
+
+    if ($passw === $passw2 && $email != null)  {
+      $sql = "INSERT INTO user_auth (USAU_ID, USAU_EMAIL, USAU_PASSWORD, USAU_USTI_ID) VALUES (NULL, '".$email."', '".$passw."', '1');";
+      $db->getConex()->query($sql);
+      
+      header("location: home.php");
+    } else {
+      $error = "El nombre o contraseña son incorrectos";
+    }  
+    $db->desconectar();
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,7 +40,7 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>Golosinas Alpha - Register</title>
+  <title>Golosinas Alpha - Registrar una cuenta</title>
 
   <!-- Custom fonts for this template-->
   <link href="../librerias/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
@@ -24,28 +55,12 @@
 
   <div class="container">
     <div class="card card-register mx-auto mt-5">
-      <div class="card-header">Register an Account</div>
+      <div class="card-header">Registrar una cuenta</div>
       <div class="card-body">
-        <form>
-          <div class="form-group">
-            <div class="form-row">
-              <div class="col-md-6">
-                <div class="form-label-group">
-                  <input type="text" id="firstName" class="form-control" placeholder="First name" required="required" autofocus="autofocus">
-                  <label for="firstName">First name</label>
-                </div>
-              </div>
-              <div class="col-md-6">
-                <div class="form-label-group">
-                  <input type="text" id="lastName" class="form-control" placeholder="Last name" required="required">
-                  <label for="lastName">Last name</label>
-                </div>
-              </div>
-            </div>
-          </div>
+        <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
           <div class="form-group">
             <div class="form-label-group">
-              <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required="required">
+              <input type="email" id="inputEmail" name="inputEmail" class="form-control" placeholder="Email address" required="required">
               <label for="inputEmail">Email address</label>
             </div>
           </div>
@@ -53,28 +68,29 @@
             <div class="form-row">
               <div class="col-md-6">
                 <div class="form-label-group">
-                  <input type="password" id="inputPassword" class="form-control" placeholder="Password" required="required">
+                  <input type="password" id="inputPassword" name="inputPassword" class="form-control" placeholder="Password" required="required">
                   <label for="inputPassword">Password</label>
                 </div>
               </div>
               <div class="col-md-6">
                 <div class="form-label-group">
-                  <input type="password" id="confirmPassword" class="form-control" placeholder="Confirm password" required="required">
+                  <input type="password" id="confirmPassword" name="confirmPassword" class="form-control" placeholder="Confirm password" required="required">
                   <label for="confirmPassword">Confirm password</label>
                 </div>
               </div>
             </div>
           </div>
-          <a class="btn btn-primary btn-block" href="login.html">Register</a>
+          <button class="btn btn-primary btn-block" type="submit"> Registrar </button>
         </form>
         <div class="text-center">
-          <a class="d-block small mt-3" href="login.html">Login Page</a>
-          <a class="d-block small" href="forgot-password.html">Forgot Password?</a>
+          <a class="d-block small mt-3" href="index.php">Ya tengo una cuenta</a>
+        </div>
+        <div class="text-danger text-center">
+            <?php echo isset($error) ? $error : ''; ?>
         </div>
       </div>
     </div>
   </div>
-
   
 
   <!-- Bootstrap core JavaScript-->
